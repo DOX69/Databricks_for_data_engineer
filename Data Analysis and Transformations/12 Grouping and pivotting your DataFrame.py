1dbb572bc60b8e5b7f2e6d51a6ec57d60432bba1
DOX69 <DOX69>
2024-05-02 06:26:40 +0000

    Join & unpivot
    Unpivot with expr()
    
    **expr("""stack(nb_columns_to_unpivot,value,column_where_value_is as (column_stack_new_col,values_stacked))""")**
    
    and remove the none value in values_stacked by **.na.drop(subset=values_stacked)** OR **filter('values_stacked is not null')**

--- Data Analysis and Transformations/12 Grouping and pivotting your DataFrame.py
+++ Data Analysis and Transformations/12 Grouping and pivotting your DataFrame.py
@@ -82,3 +82,63 @@ countries.groupBy("REGION_ID","SUB_REGION_ID")\
 countries.groupBy("SUB_REGION_ID")\
     .pivot("REGION_ID")\
     .agg(sum("POPULATION")).display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Unpivotting
+
+# COMMAND ----------
+
+pivot_countries = countries.groupBy("SUB_REGION_ID")\
+    .pivot("REGION_ID")\
+    .agg(sum("POPULATION"))
+
+# COMMAND ----------
+
+pivot_countries.display()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import expr
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Rename withColumsRenamed({name_old:name_new,:,:})
+
+# COMMAND ----------
+
+ pivot_countries= pivot_countries\
+    .withColumnsRenamed({'10': 'REGION_10',
+                        '20': 'REGION_20',
+                        '30': 'REGION_30',
+                        '40': 'REGION_40',
+                        '50': 'REGION_50'},
+                        )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Unpivot with expr()
+# MAGIC
+# MAGIC **expr("""stack(nb_columns_to_unpivot,value,column_where_value_is as (column_stack_new_col,values_stacked))""")**
+# MAGIC
+# MAGIC and remove the none value in values_stacked by **.na.drop(subset=values_stacked)** OR **filter('values_stacked is not null')**
+
+# COMMAND ----------
+
+pivot_countries\
+    .select('SUB_REGION_ID',expr("""stack(5,
+                                 '10',REGION_10,
+                                 '20',REGION_20,
+                                 '30',REGION_30,
+                                 '40',REGION_40,
+                                 '50',REGION_50) 
+                                 as (REGION_ID,POPULATION)"""))\
+                                .na.drop(subset='POPULATION').display()
