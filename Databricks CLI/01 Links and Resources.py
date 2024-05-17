23a7c22387af4f6a94a10143ed6de0bd62393b6c
DOX69 <DOX69>
2024-05-17 05:21:31 +0000

    Ressources for databricks CLI

---
+++ Databricks CLI/01 Links and Resources.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Links and Resources
+# MAGIC
+# MAGIC #### Resources
+# MAGIC * Databricks CLI https://learn.microsoft.com/en-us/azure/databricks/dev-tools/cli/
