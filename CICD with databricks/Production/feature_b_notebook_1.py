936612c6bf34942b42f473a685aac5c31167daec
DOX69 <DOX69>
2024-05-21 06:31:58 +0000

    CD : Production

---
+++ CICD with databricks/Production/feature_b_notebook_1.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+print('this is feature b in notebook 1')
+
+# COMMAND ----------
+
+print("let's update this notebook to see what happen when there is a conflict")
---
+++ CICD with databricks/Production/feature_b_notebook_2.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+print('Hello from feature b notebook 2')
+print('I updated this notebook so the pipeline will be trigger and deploy notebooks in test and production stage')
